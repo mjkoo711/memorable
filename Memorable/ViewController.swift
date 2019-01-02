06a8debde060e3e29e8fd0c3da3c3f4d58efcc5c
KooMinJun <mjkoo711@gmail.com>
2019-01-03 02:03:24 +0900

    폴더구조 변경

---
+++ Memorable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Memorable
+//
+//  Created by 구민준 on 03/01/2019.
+//  Copyright © 2019 mjkoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
